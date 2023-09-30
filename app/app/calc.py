ee8bc137c18a5706848a101d1e0915ed0b0644a7
guhank <guhan.k@siamcomputing.com>
2023-09-30 20:22:24 +0530

    db config

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+"""
+simple calc for testing 
+"""
+
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+"""
+sample test.
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    def test_add_number(self):
+        res = calc.add(5,6)
+        self.assertEqual(res, 11)
+    def test_subtract_number(self):
+        res = calc.subtract(15, 10)
+        self.assertEqual(res, 5)
