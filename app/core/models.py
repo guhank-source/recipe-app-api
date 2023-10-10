6db2b36acb8c159a0e8f9be179ff8a5f33539ee0
guhank <guhan.k@siamcomputing.com>
2023-10-10 20:42:36 +0530

    add

--- app/core/models.py
+++ app/core/models.py
@@ -10,7 +10,7 @@ from django.contrib.auth.models import (
 )
 class UserManager(BaseUserManager):
     """Manager for users"""
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, password = None, **extra_fields):
         """Create, save and return a new user"""
         if not email:
             raise ValueError('User must have an email address')
@@ -45,9 +45,7 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
-    tags = models.ManyToManyField(
-        'Tag',
-    )
+    tags = models.ManyToManyField('Tag')
 
     def __str__(self):
         return self.title
@@ -56,7 +54,7 @@ class Tag(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
 
     def __str__(self):
