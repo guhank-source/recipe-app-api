06e9ef85265a0a102efd4140c58120ff180b32b1
guhank <guhan.k@siamcomputing.com>
2023-10-28 13:04:11 +0530

    dd

--- proxy/run.sh
+++ proxy/run.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-envsubst < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-nginx -g "daemon off;"
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+nginx -g 'daemon off;'
